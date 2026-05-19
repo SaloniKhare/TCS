2a643e8beae086002dc0e822cd18404ab736faef
Saloni Khare <salonikhare21@gmail.com>
2026-05-19 13:12:04 +0530

    Update Rotate_Left.py

--- Array/Rotate_Left.py
+++ Array/Rotate_Left.py
@@ -1,8 +1,10 @@
 ar=[0,1,2,0,3,0,0]
 arr=[1,2,3,4,5,6,7]
 k=3
-arr[:3]=arr[:3][::-1]
-arr[3:]=arr[3:][::-1]
+if k>len(arr):
+        print(-1)
+arr[:k]=arr[:k][::-1]
+arr[k:]=arr[k:][::-1]
 arr.reverse()
 print(arr)
         
