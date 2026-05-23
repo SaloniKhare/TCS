7a04257c6728435fd4ba8e6e656e9e36741ad53c
Saloni Khare <salonikhare21@gmail.com>
2026-05-23 15:04:40 +0530

    Add majorityElement method to Solution class

--- Hashing/Majority_Element.py
+++ Hashing/Majority_Element.py
@@ -8,3 +8,8 @@ class Solution:
         for k,v in h.items():
             if v>s:
                 return k
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        nums.sort()
+        return nums[len(nums) // 2]
