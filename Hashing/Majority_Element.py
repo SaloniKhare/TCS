33d36b76ce00755d61b537a263efcfc06e6f160a
Saloni Khare <salonikhare21@gmail.com>
2026-05-23 15:03:42 +0530

    Implement majority element finder using Counter

---
+++ Hashing/Majority_Element.py
@@ -0,0 +1,10 @@
+from collections import Counter
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        h=Counter(nums)
+        n=len(nums)
+        s=n/2
+        
+        for k,v in h.items():
+            if v>s:
+                return k
