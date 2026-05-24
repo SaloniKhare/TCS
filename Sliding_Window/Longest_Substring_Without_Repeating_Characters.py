f6e296302f82627151b606f4ff1368bed38d8a8e
Saloni Khare <salonikhare21@gmail.com>
2026-05-24 22:54:41 +0530

    Implement longestUniqueSubstr function
    Added function to find the length of the longest substring without repeating characters.

--- Sliding_Window/Longest_Substring_Without_Repeating_Characters.py
+++ Sliding_Window/Longest_Substring_Without_Repeating_Characters.py
@@ -12,4 +12,13 @@ class Solution:
         return ans
 
 
-
+def longestUniqueSubstr(s):
+    n = len(s)
+    res = 0
+    lastIndex = [-1] * 26
+    start = 0
+    for end in range(n):
+        start = max(start, lastIndex[ord(s[end]) - ord('a')] + 1)
+        res = max(res, end - start + 1)
+        lastIndex[ord(s[end]) - ord('a')] = end
+    return res
