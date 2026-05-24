4c07adcf9e6d2ac346a4d56754605fcdb3f4638b
Saloni Khare <salonikhare21@gmail.com>
2026-05-24 20:28:34 +0530

    Refactor longestCommonPrefix method for clarity

--- String/Longest_Common_Prefix.py
+++ String/Longest_Common_Prefix.py
@@ -9,3 +9,20 @@ class Solution:
                 return ans
             ans+=a[i]
         return ans
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        a = min(strs)
+        b = max(strs)
+        
+        ans = ""
+        # Compare them just like you did before
+        for i in range(min(len(a), len(b))):
+            if a[i] != b[i]:
+                return ans
+            ans += a[i]
+            
+        return ans
